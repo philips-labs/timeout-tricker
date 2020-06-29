c226ba8fa837e51a6b9efb8685a14c583838c225
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-06-29 22:41:39 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:1.14.4 as builder
+WORKDIR /build
+COPY go.mod .
+COPY go.sum .
+RUN go mod download
+
+# Build
+COPY . .
+RUN CGO_ENABLED=0 go build -o timeout-tricker
+
+FROM alpine:latest 
+RUN apk update && apk add ca-certificates && rm -rf /var/cache/apk/*
+WORKDIR /app
+COPY --from=builder /build/timeout-tricker /app
+EXPOSE 8080
+CMD ["/app/timeout-tricker"]
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"os"
+	"strconv"
+	"time"
+)
+func main() {
+	// Config
+	host := os.Getenv("HOST")
+	if host == "" {
+		fmt.Printf("missing host")
+	}
+	timeout := os.Getenv("TIMEOUT")
+	scheme := os.Getenv("SCHEME")
+	if scheme == "" {
+		scheme = "http"
+	}
+	proxyTimeout, err := strconv.Atoi(timeout)
+	if err != nil {
+		fmt.Printf("invalid timeout: %v\n", err)
+		return
+	}
+
+	// Server
+	e := echo.New()
+	e.Any("/*", timeoutFixerFor(host, scheme, proxyTimeout))
+	e.Start(":8080")
+}
+
+func timeoutFixerFor(host, scheme string, timeout int) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		director := func(req *http.Request) {
+			r := c.Request()
+			req = r
+			req.URL.Scheme = scheme
+			req.URL.Host = r.URL.Host
+		}
+		req := c.Request()
+		req.URL.Host = host
+		req.Host = host
+		req.URL.Scheme = scheme
+
+		proxy := &httputil.ReverseProxy{Director: director}
+		recorder := httptest.NewRecorder()
+		done := make(chan bool)
+
+		// This might take up to an hour, so spawn a go routine
+		go func() {
+			proxy.ServeHTTP(recorder, req)
+			done <- true
+		}()
+
+		// Write headers
+		// We may have to use some heuristics based on the request
+		// to send the correct headers
+
+		for {
+			select {
+			case <-done:
+				// Upstream request is done!
+				// Write out the original body
+				fmt.Printf("remote request finished\n")
+				_, err := c.Response().Writer.Write(recorder.Body.Bytes())
+				return err
+			case <-time.After(time.Duration(timeout) * time.Second):
+				writer := c.Response().Writer
+				_, _ = writer.Write([]byte(" "))
+				if f, ok := writer.(http.Flusher); ok {
+					fmt.Printf("flushing\n")
+					f.Flush()
+				}
+			}
+		}
+	}
+}
